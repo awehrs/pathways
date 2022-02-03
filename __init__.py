2f9e8dfcff9a5dc9b619642bc9f6af9370ef1843
awehrs <43420108+awehrs@users.noreply.github.com>
2022-02-03 12:01:03 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,1 @@
-download_config = tfds.download_config(
-    extract_dir=extract_dir,
-    manual_dir=manual_dir,
-)
-ds = tfds.load(
-    "imagenet2012:5.*.*",
-    data_dir=data,
-    download_config=None,
-)
+
